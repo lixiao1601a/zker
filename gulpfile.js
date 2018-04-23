4af0ba07d7b83f4f4c169581dcf6cb76b4567629
lixiao1601a <24881138732@qq.com>
2018-04-23 10:12:42 +0800

    nexttijiao

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var webserver = require("gulp-webserver");
 var connect = require("gulp-connect");
+var objAjax = require("./src/data/data.json")
 
 gulp.task("reloadPage", function() {
     gulp.src(".")
@@ -10,4 +11,19 @@ gulp.task("reloadPage", function() {
 gulp.task("watch", function() {
     gulp.watch("./index.html", ["reloadPage"])
 })
-gulp.task("default", ["reloadPage"])
+
+gulp.task("webserver", function() {
+    gulp.src(".")
+        .pipe(webserver({
+            port: 8080,
+            livereload: true,
+            middleware: function(req, res, next) {
+                if (req.url === "/index") {
+                    res.end(JSON.stringify(objAjax))
+                }
+                next()
+            }
+        }))
+})
+
+gulp.task("default", ["reloadPage", "watch", "webserver"])
